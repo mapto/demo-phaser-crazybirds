fdcbc6d18460172b31644cdd0a9857418447fe70
Martin Ruskov <martin.ruskov@my-invenio.com>
2019-07-30 18:51:07 +0200

    added grunt for development environment

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['.', '<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    connect: {
+        server: {
+            options: {
+                port: 3000, // custom port
+                base: '.', // current directory for 'index.html' is root
+                keepalive: true, // keep the server alive indefinitely
+                livereload: true,
+                open: {target: 'http://localhost:3000'}
+                }
+            }
+        }
+	});
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('development', [
+  	'jshint',
+  	'connect:server',
+  	'watch',
+  ]);
+
+  grunt.registerTask('default', ['development']);
+
+};
